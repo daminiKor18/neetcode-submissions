830b4ed7d729b7e5808bd83fe2244574800df77a
Damini Koranga <149085643+daminiKor18@users.noreply.github.com>
2026-07-24 13:48:12 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int candidate1=0,candidate2=0;
+        
+        int count1=0,count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+
+             if(nums[i]==candidate1)
+            {
+                count1++;
+            }
+             else if(nums[i]==candidate2)
+            {
+                count2++;
+            }
+             else if(count1==0) 
+            {
+                candidate1=nums[i];
+                count1=1;
+            }
+            else if(count2==0)
+            {
+                candidate2=nums[i];
+                count2=1;
+            }
+            
+            else
+            {
+                count1--;
+                count2--;
+            }
+
+        }
+        count1=0;
+        count2=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==candidate1)
+            {
+                count1++;
+            }
+            else if(nums[i]==candidate2)
+            {
+                count2++;
+            }
+
+        }
+        vector<int>ans;
+        if(count1>nums.size()/3)
+        {
+        ans.push_back(candidate1);
+        }
+        if(count2>nums.size()/3)
+        {
+            ans.push_back(candidate2);
+        }
+        return ans;
+    }
+};
